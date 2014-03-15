f685b0f9171398c18384c594c70433af51316075
Adam Kurkiewicz <adam@kurkiewicz.pl>
2014-03-15 22:10:41 +0000

    maybe this

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,16 +25,21 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'database/database.sqlite3'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default='postgres://foo:bar@localhost:5432/db')}
+
+#DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'postgresql_psycopg2' #'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': os.path.join(BASE_DIR, 'database/#database.sqlite3'),                      # Or path to database file #if using sqlite3.
+#        'USER': '',                      # Not used with sqlite3.
+#        'PASSWORD': '',                  # Not used with sqlite3.
+#        'HOST': '',                      # Set to empty string for #localhost. Not used with sqlite3.
+#        'PORT': '',                      # Set to empty string for #default. Not used with sqlite3.
+#    }
+#}
 
 
 # Local time zone for this installation. Choices can be found here:
