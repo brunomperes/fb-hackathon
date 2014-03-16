e520a271f21b1c75345d29be020daf537bf47911
Bruno Peres <brunomacielp93@gmail.com>
2014-03-16 03:05:36 +0000

    Removed allauth

--- myproject/settings.py
+++ myproject/settings.py
@@ -151,8 +151,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     # Required by allauth template tags
     "django.core.context_processors.request",
     # allauth specific context processors
-    "allauth.account.context_processors.account",
-    "allauth.socialaccount.context_processors.socialaccount",
     "django.contrib.auth.context_processors.auth",
 )
 
