ee2da0e7ab649d704108fb30ff57170f7f976708
Bruno Peres <brunomacielp93@gmail.com>
2014-03-16 00:22:30 +0000

    Enabled admin

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
+	url(r'^admin/', include(admin.site.urls)),
 	(r'^myapp/', include('myproject.myapp.urls')),
 	(r'^$', RedirectView.as_view(url='/myapp/list/')), # Just for ease of use.
 )
