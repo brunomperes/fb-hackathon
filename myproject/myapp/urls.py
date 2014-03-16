ca82ccf031dd5bfe427eb85bee423a1c09704a64
Bruno Peres <brunomacielp93@gmail.com>
2014-03-16 00:55:16 +0000

    Removed wtf

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('myproject.myapp.views',
 	url(r'^$', 'index', name='index'),
     url(r'^ready/$', 'ready', name='ready'),
-    url(r'^wtf/$', 'wtf', name='wtf'),
     url(r'^game/$', 'game', name='game'),
     url(r'^list/$', 'list', name='list'),
     url(r'^login/$', 'login', name='login'),
